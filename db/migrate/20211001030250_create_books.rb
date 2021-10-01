7541b2032a09a0bd61a0063fcc8a1e648f3af295
root <root@localhost>
2021-10-01 04:30:36 +0000

    for test

--- db/migrate/20211001030250_create_books.rb
+++ db/migrate/20211001030250_create_books.rb
@@ -2,6 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
+      t.string :author
+      t.date :published_date
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
